832cdf148594c0d12f5ab38ad99ec2c38d423828
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-10-21 23:19:12 +0530

    Update config.php

--- Documents/Sign In/config.php
+++ Documents/Sign In/config.php
@@ -6,7 +6,9 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Wethebestmusix.123');
 define('DB_NAME', 'demo');
  
+require_once "../database_relay.php";
+
 /* Attempt to connect to MySQL database */
 $DB_Manager = new DB_Relay(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$link = $DB_Manager->GetDatabaseLink();
+$link = $DB_Manager->GetLink();
 ?>
